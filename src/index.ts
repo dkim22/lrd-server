34a28e9180386f9623283a638e9b9909cfe154a4
dkim22 <ehddnr1186@gmail.com>
2020-10-17 20:22:10 +0900

    Logout

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import redis from 'redis'
 import session from 'express-session'
 import connectRedis from 'connect-redis'
 import cors from 'cors'
-import { __prod__ } from './constants'
+import { COOKIE_NAME, __prod__ } from './constants'
 import microConfig from './mikro-orm.config'
 import { HelloResolver } from './resolvers/hello'
 import { PostResolver } from './resolvers/post'
@@ -31,7 +31,7 @@ const main = async () => {
 
   app.use(
     session({
-      name: 'qid',
+      name: COOKIE_NAME,
       store: new RedisStore({
         client: redisClient,
         disableTouch: true,
